0f7b7bc4d9ba4818e2b9473caf0da403dd65b2c5
kaviarasuns <kaviarasu_ns@icloud.com>
2024-06-25 16:23:52 +0530

    day one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,23 @@ import {
   SignOutButton,
   SignedIn,
   SignedOut,
+  useOrganization,
   useSession,
+  useUser,
 } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
-  const files = useQuery(api.files.getFile);
+  const organization = useOrganization();
+  const user = useUser();
+
+  let orgId: string | undefined;
+  if (organization.isLoaded && user.isLoaded) {
+    orgId = organization.organization?.id ?? user.user?.id;
+  }
+
+  const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
   const createFile = useMutation(api.files.createFile);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -29,7 +39,13 @@ export default function Home() {
       {files?.map((file) => {
         return <div key={file._id}>{file.name}</div>;
       })}
-      <Button onClick={() => createFile({ name: "Hello World" })}>
+      <Button
+        onClick={() => {
+          console.log(organization);
+          if (!orgId) return;
+          createFile({ name: "Hello World", orgId });
+        }}
+      >
         Click Me
       </Button>
     </main>
